52170ed7a4065adacabd37eaae36d2bf854f6df2
SetyoRian <setyo.wahyu.trianto@gmail.com>
2023-01-10 12:07:03 +0700

    first commit

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+let shortUrl = 1;
+let originUrl = null;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +23,16 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', function(req, res) {
+  let origin = req.body.url;
+  originUrl = origin;
+  res.json({ original_url: origin, short_url: shortUrl });
+});
+
+app.get(`/api/shorturl/${shortUrl}`, function(req, res) {
+  res.redirect(originUrl);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
